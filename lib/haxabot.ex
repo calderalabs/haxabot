8cd7f8ac655a7f4558a31c909e9212d90fc1c905
Ju Liu <ju.liu@welaika.com>
2017-05-26 19:36:28 +0100

    Initial commit

---
+++ lib/haxabot.ex
@@ -0,0 +1,18 @@
+defmodule Haxabot do
+  @moduledoc """
+  Documentation for Haxabot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Haxabot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/haxabot_test.exs
@@ -0,0 +1,8 @@
+defmodule HaxabotTest do
+  use ExUnit.Case
+  doctest Haxabot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
