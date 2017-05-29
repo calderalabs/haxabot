83848fb04b63cdc83266c5a0c1fdbf7116f4057b
Ju Liu <ju.liu@welaika.com>
2017-05-29 20:57:38 +0100

    Clean top level module

--- lib/haxabot.ex
+++ lib/haxabot.ex
@@ -1,18 +1,2 @@
 defmodule Haxabot do
-  @moduledoc """
-  Documentation for Haxabot.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Haxabot.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
