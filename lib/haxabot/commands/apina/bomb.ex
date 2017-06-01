4c63dfb6c04151b5a4b0472097d32c6b06ee6bc9
Ju Liu <ju.liu@welaika.com>
2017-06-01 09:47:07 +0100

    Stream apina bombs

--- lib/haxabot/commands/apina/bomb.ex
+++ lib/haxabot/commands/apina/bomb.ex
@@ -11,14 +11,19 @@ defmodule Haxabot.Commands.Apina.Bomb do
   defp do_run(count, message, state) when count < @bomb_limit do
     Stream.repeatedly(&Haxabot.Commands.Apina.get_random_url/0)
     |> Enum.reduce_while(MapSet.new, fn url, acc ->
-      new_acc = MapSet.put(acc, url)
-      case MapSet.size(new_acc) do
-        ^count -> {:halt, new_acc}
-        _other -> {:cont, new_acc}
+      if MapSet.member?(acc, url) do
+        {:cont, acc}
+      else
+        new_acc = MapSet.put(acc, url)
+        send_message(url, message.channel, state)
+
+        case MapSet.size(new_acc) do
+          ^count ->
+            {:halt, new_acc}
+          _other ->
+            {:cont, new_acc}
+        end
       end
-    end)
-    |> Enum.each(fn url ->
-      send_message(url, message.channel, state)
     end)
 
     {:ok, state}
